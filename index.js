3237879e9b1f59589b1549a89a7fe300721f6878
NazPro <najmulbinnurul3@gmail.com>
2022-12-18 11:15:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app=express()
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port=process.env.PORT || 5000;
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res)=>{
+    res.send('server is running')
+})
+
+app.listen(port, (req, res)=>{
+    console.log('listening to port', port);
+})
+
+
+// mongo
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.u5tj5cw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        const serviceCollection=client.db('Rooftop-doctor').collection('services')
+    }
+    finally{
+
+    }
+}
+run().catch(err=>{})
+
+
